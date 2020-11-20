5a9b035b2ed0a372859ddf3a1496fbe2f23b6aa1
AMR692 <amr@andrewmartinross.com>
2020-11-19 21:09:25 -0500

    Introduce random error into π

--- the_mathematician.py
+++ the_mathematician.py
@@ -1,6 +1,7 @@
 #!/usr/local/bin/python3
 
 from re import sub
+from random import choice, randrange
 
 def WordCount(x):
 	x = x.lower()
@@ -23,7 +24,16 @@ numberToWord = {'.': 'point',
 intro = 'This is the introduction to the story. "'
 outro = '," and that\'s the end of the story.'
 
-digitsNeeded = 1000 - (WordCount(intro) + WordCount(outro))
+digitsNeeded = 50000 - (WordCount(intro) + WordCount(outro))
+
+# is this version going to have an error in π?
+errorPct = 34 # this % of text generated will have an error in π
+if randrange(0, 100) < errorPct:
+	# okay, there will be an error. Now to figure out where the error will be
+	errorStart = digitsNeeded // 3 # we don't want the error to appear too soon
+	errorEnd = digitsNeeded - errorStart # or too close to the end. We want to be in the middle 3rd
+	errorLocation = randrange(errorStart, errorEnd)
+	print(errorStart, errorLocation, errorEnd)
 
 # reading in a 1M-byte text file all at once probably isn't the best idea,
 # so we're going to read the file one character at a time.
@@ -35,12 +45,19 @@ piFile = open('pi_dec_1m.txt')
 for i in range(digitsNeeded):
 	nextChar = piFile.read(1)
 	if nextChar:
-		if i == 0:
-			print(numberToWord[nextChar].capitalize(), end = ' ')
+		correctDigit = numberToWord[nextChar]
+		if i == errorLocation:
+			numberList = list(numberToWord.values())
+			numberList.remove('point')
+			numberList.remove(correctDigit)
+			wrongDigit = choice(numberList)
+			print(wrongDigit, end = ' ')
+		elif i == 0:
+			print(correctDigit.capitalize(), end = ' ')
 		elif i == (digitsNeeded - 1):
-			print(numberToWord[nextChar], end = '')
+			print(correctDigit, end = '')
 		else:
-			print(numberToWord[nextChar], end = ' ')
+			print(correctDigit, end = ' ')
 	else:
 		print('End of file error. I guess π isn\'t infinite after all! ¯\\_(ツ)_/¯')
 
